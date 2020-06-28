145f26ef7f3930d6f919de1de026e2ed6f6c0dd5
Liu Shike <liushike1997@gmail.com>
2020-06-28 17:30:40 +0800

    ad

--- test/html/arrowfunc.js
+++ test/html/arrowfunc.js
@@ -3,3 +3,23 @@ console.log( func(1,2,3));
 
 let double = n => n * 2;
 console.log( double(3) );
+
+let sayHi = () => alert("Hello");
+
+sayHi();
+
+debugger;
+
+let age = prompt('age?', 18);
+
+let welcome = (age < 18) ?
+    () => alert('hello') :
+    () => alert('good');
+welcome();
+
+function ask(question, yes, no) {
+  if (confirm(question)) yes()
+  else no();
+}
+
+ask("are u ok?", () => alert('ok'), () =>(alert('no')));
